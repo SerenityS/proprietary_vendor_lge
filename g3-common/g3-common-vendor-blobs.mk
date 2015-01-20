9dbe47d01335af08bd0f792dedfb9361d4aa93e0
SerenityS <jins4218@naver.com>
2015-01-20 13:16:28 +0900

    f400s: intial support

--- g3-common/g3-common-vendor-blobs.mk
+++ g3-common/g3-common-vendor-blobs.mk
@@ -219,6 +219,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g3-common/proprietary/vendor/lib/libthermalioctl.so:system/vendor/lib/libthermalioctl.so \
     vendor/lge/g3-common/proprietary/bin/time_daemon:system/bin/time_daemon \
     vendor/lge/g3-common/proprietary/vendor/lib/libTimeService.so:system/vendor/lib/libTimeService.so \
+    vendor/lge/g3-common/proprietary/vendor/lib/libtime_genoff.so:obj/lib/libtime_genoff.so \
     vendor/lge/g3-common/proprietary/vendor/firmware/wcnss.b00:system/vendor/firmware/wcnss.b00 \
     vendor/lge/g3-common/proprietary/vendor/firmware/wcnss.b01:system/vendor/firmware/wcnss.b01 \
     vendor/lge/g3-common/proprietary/vendor/firmware/wcnss.b02:system/vendor/firmware/wcnss.b02 \
